b8013506ee2aa27f492139f9b1758b663b3a0d9a
poymanov <n.poymanov@gmail.com>
2022-01-26 19:59:04 +0300

    3-module-4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filteredUsers = users.filter((user) => user.age <= age);
+
+  return filteredUsers.reduce(function(previousValue, item, index) {
+    previousValue += item.name + ', ' + item.balance;
+
+    if (index < array.length - 1) {
+      previousValue += '\n';
+    }
+
+    return previousValue;
+  }, '');
 }
