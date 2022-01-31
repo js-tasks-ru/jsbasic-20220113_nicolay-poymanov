03f0cdd592c5b6a923dd9b6ee0253a5d4fd8f71d
poymanov <n.poymanov@gmail.com>
2022-01-31 23:14:41 +0300

    4-module-3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.rows;
+  const rowsCount = rows.length;
+
+  for (let i = 0; i < rowsCount; i++) {
+    const currentRow = rows[i];
+    const availableValue = currentRow.cells[3].dataset.available;
+    const genderValue = currentRow.cells[2].innerText;
+    const ageValue = parseInt(currentRow.cells[1].innerText);
+
+    if (availableValue === 'true') {
+      currentRow.classList.add('available');
+    } else if (availableValue === 'false') {
+      currentRow.classList.add('unavailable');
+    } else {
+      currentRow.setAttribute('hidden', true);
+    }
+
+    if (genderValue === 'm') {
+      currentRow.classList.add('male');
+    } else if (genderValue === 'f') {
+      currentRow.classList.add('female');
+    }
+
+    if (ageValue < 18) {
+      currentRow.style.textDecoration = 'line-through';
+    }
+  }
 }
