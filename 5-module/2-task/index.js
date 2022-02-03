b05a11edbb51ea7b964bdaa8305fe07215679a08
poymanov <n.poymanov@gmail.com>
2022-02-03 22:27:26 +0300

    5-module-2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,6 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('#text');
+
+  button.addEventListener('click', () => text.toggleAttribute('hidden'));
 }
