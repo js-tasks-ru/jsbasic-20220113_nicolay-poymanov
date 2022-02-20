2945fffce1030752ed2061b230d7e4c39e46f898
poymanov <n.poymanov@gmail.com>
2022-02-20 21:24:44 +0300

    8-module-3-task

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,29 +6,83 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {
+      return null;
+    }
+
+    let cartItem = this.findCartItemByProductId(product.id);
+
+    if (cartItem) {
+      cartItem.count++;
+    } else {
+      cartItem = {
+        product,
+        count: 1,
+      };
+
+      this.cartItems.push(cartItem);
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItem = this.findCartItemByProductId(productId);
+
+    if (!cartItem) {
+      return null;
+    }
+
+    let newCount;
+
+    if (amount === 1) {
+      newCount = ++cartItem.count;
+    } else if (amount === -1) {
+      newCount = --cartItem.count;
+    } else {
+      return null;
+    }
+
+    if (newCount < 0) {
+      return null;
+    }
+    else if (newCount === 0) {
+      this.cartItems = this.cartItems.filter((currentCartItem) => {
+        return currentCartItem.id !== cartItem.id;
+      });
+    } else {
+      cartItem.count = newCount;
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((totalCount, cartItem) => {
+      return totalCount + cartItem.count;
+    }, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((totalPrice, cartItem) => {
+      return totalPrice + (cartItem.product.price * cartItem.count);
+    }, 0);
   }
 
   onProductUpdate(cartItem) {
     // реализуем в следующей задаче
 
     this.cartIcon.update(this);
+  }
+
+  findCartItemByProductId(productId) {
+    return this.cartItems.find((cartItem) => {
+      return cartItem.product.id === productId;
+    });
   }
 }
 
